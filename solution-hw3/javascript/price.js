7d5ddb3a2ec642608cfb40c1add4b0eb7eaba487
mhcisoniawu <112278648+mhcisoniawu@users.noreply.github.com>
2022-09-22 15:34:15 -0400

    comments and minor changes

--- solution-hw3/javascript/price.js
+++ solution-hw3/javascript/price.js
@@ -1,8 +1,4 @@
-// Initializing
-let glazingIndex = 0;
-let packIndex = 0;
-
-// An array of glazing price adaptaions
+// Declare an array of glazing price adaptaions
 let glazingPriceAdapt = [
   {
     glazing: "Original",
@@ -22,40 +18,49 @@ let glazingPriceAdapt = [
   },
 ];
 
-// An array of pack price adaptaions
+// Declare an array of pack price adaptaions
 let packPriceAdapt = [
   {
-    size: 1, 
+    size: "1",
     price: 1,
   },
   {
-    size: 
-    price: 2,
-  },
-  {
+    size: "3",
     price: 3,
   },
   {
-    price: 4,
+    size: "6",
+    price: 5,
+  },
+  {
+    size: "12",
+    price: 10,
   },
 ];
 
-function generatePrice(glazingIndex, packIndex) {
+// Initializing the indexes
+let glazingIndex = 0;
+let packIndex = 0;
+
+// Take the price from adpatation arrays and calculate the total amount of money
+function calculateTotal(glazingIndex, packIndex) {
   let cusPrice = document.querySelector(".cusPrice");
   let glazingPrice = glazingPriceAdapt[glazingIndex].price;
   let packPrice = packPriceAdapt[packIndex].price;
-
-  cusPrice.innerText = (2.49 + glazingPrice) * packPrice;
+  // Update the total price and round to 2 decimals
+  cusPrice.innerText = ((2.49 + glazingPrice) * packPrice).toFixed(2);
 }
 
+// Runs whenever the glazing dropdown's value changes
 function onSelectValueChangeGlazing() {
   glazingIndex = parseInt(this.value);
-  generatePrice(glazingIndex, packIndex);
+  calculateTotal(glazingIndex, packIndex);
 }
 
+// Runs whenever the pack dropdown's value changes
 function onSelectValueChangePack() {
   packIndex = parseInt(this.value);
-  generatePrice(glazingIndex, packIndex);
+  calculateTotal(glazingIndex, packIndex);
 }
 
 // Find the selected glazing when productDetail page loads
@@ -63,9 +68,10 @@ let selectGlazing = document.querySelector("#glazingOptions");
 // Find the selected pack size when productDetail page loads
 let selectPack = document.querySelector("#packOptions");
 
-// This is a listener that runs when the user make changes in the glazing dropdown.
+// This is a listener that runs when the user make changes in the glazing dropdown
 selectGlazing.addEventListener("change", onSelectValueChangeGlazing);
-// This is a listener that runs when the user make changes in the pack dropdown.
-selectPack.addEventListener("change,", onSelectValueChangePack);
+// This is a listener that runs when the user make changes in the pack dropdown
+selectPack.addEventListener("change", onSelectValueChangePack);
 
-generatePrice(glazingIndex, packIndex);
+// Call the function to get the total price
+calculateTotal(glazingIndex, packIndex);
