c582d9f7df6b4878842be065c7f6c21f5ad1450d
mhcisoniawu <112278648+mhcisoniawu@users.noreply.github.com>
2022-09-22 11:14:35 -0400

    changes

--- solution-hw3/javascript/price.js
+++ solution-hw3/javascript/price.js
@@ -1,24 +1,71 @@
-// selecting glazing dropdown //
-// let glazingOptions = document.getElementById("glazingOptions");
-// let glazingPrice = glazingOptions.value;
+// Initializing
+let glazingIndex = 0;
+let packIndex = 0;
 
-// selecting pack dropdown //
-// let packOptions = document.getElementById("packOptions");
-// let packPrice = packOptions.value;
-// selecting the total price for the current cinnamon roll customization //
-let glazingPrice;
-let packPrice;
-let cusPrice = document.querySelector("cusPrice");
+// An array of glazing price adaptaions
+let glazingPriceAdapt = [
+  {
+    glazing: "Original",
+    price: 0.0,
+  },
+  {
+    glazing: "Sugar Milk",
+    price: 0.0,
+  },
+  {
+    glazing: "Vanilla Milk",
+    price: 0.5,
+  },
+  {
+    glazing: "Double Chocolate",
+    price: 1.5,
+  },
+];
 
-function selectGlazing(index) {
-  let selectedGlazing = document.getElementById("glazingOptions").value;
-  // glazingPrice = selectedGlazing;
+// An array of pack price adaptaions
+let packPriceAdapt = [
+  {
+    size: 1, 
+    price: 1,
+  },
+  {
+    size: 
+    price: 2,
+  },
+  {
+    price: 3,
+  },
+  {
+    price: 4,
+  },
+];
+
+function generatePrice(glazingIndex, packIndex) {
+  let cusPrice = document.querySelector(".cusPrice");
+  let glazingPrice = glazingPriceAdapt[glazingIndex].price;
+  let packPrice = packPriceAdapt[packIndex].price;
+
+  cusPrice.innerText = (2.49 + glazingPrice) * packPrice;
 }
-console.log(selectGlazing(3));
 
-function selectPack() {
-  let selectedPack = document.getElementById("packOptions").value;
-  // packPrice = selectedPack;
+function onSelectValueChangeGlazing() {
+  glazingIndex = parseInt(this.value);
+  generatePrice(glazingIndex, packIndex);
 }
 
-selectedPrice = (2.49 + glazingPrice) * packPrice;
+function onSelectValueChangePack() {
+  packIndex = parseInt(this.value);
+  generatePrice(glazingIndex, packIndex);
+}
+
+// Find the selected glazing when productDetail page loads
+let selectGlazing = document.querySelector("#glazingOptions");
+// Find the selected pack size when productDetail page loads
+let selectPack = document.querySelector("#packOptions");
+
+// This is a listener that runs when the user make changes in the glazing dropdown.
+selectGlazing.addEventListener("change", onSelectValueChangeGlazing);
+// This is a listener that runs when the user make changes in the pack dropdown.
+selectPack.addEventListener("change,", onSelectValueChangePack);
+
+generatePrice(glazingIndex, packIndex);
